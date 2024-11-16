dbcf882443cf00ed151905d609545731e061ca6b
cilegordev <123652808+cilegordev@users.noreply.github.com>
2024-11-17 03:31:06 +0700

    i don't know what i'm doing fix script add open as root shortcut fix waybar color update icon

--- .config/hypr/backend.sh
+++ .config/hypr/backend.sh
@@ -3,9 +3,10 @@
 # Still Remember Should I Use Kali Linux?
 # DOC https://www.kali.org/docs/introduction/should-i-use-kali-linux/
 
-echo PRIVACY LOCKED 
+echo DISABLE UNUSED PROPERTY 
 sleep 1
 sudo rmmod -f pcspkr
+# sudo rmmod -f snd_pcm
 # amixer sset Capture 25000
 amixer sset Capture 0%
 amixer sset Capture nocap
@@ -13,8 +14,6 @@ amixer sset Master 75%
 
 sleep 1
 sudo rmmod -f uvcvideo
-# sleep 1
-# sudo rmmod -f snd_pcm
 sudo rmmod -f bluetooth
 sudo rfkill block bluetooth
 sudo systemctl disable bluetooth
@@ -24,7 +23,7 @@ sudo /etc/init.d/cups stop
 sudo systemctl disable ssh.service
 sudo /etc/init.d/ssh stop
 
-echo TURN ON PORTAL HYPRLAND 
+echo TURN ON PORTAL HYPRLAND 
 sleep 1
 killall -e xdg-desktop-portal-wlr
 killall xdg-desktop-portal
@@ -36,11 +35,8 @@ sleep 1
 /usr/lib/xdg-desktop-portal-hyprland &
 /usr/libexec/xdg-desktop-portal-hyprland &
 
-sleep 1
-/usr/lib/xdg-desktop-portal-hyprland &
-/usr/libexec/xdg-desktop-portal &
-
+echo "\e[1;31mWarning: you are using the root account. You may harm your system 💀\e[0m"
 sleep 1
 xhost +si:localuser:root &
-echo "\e[1;31mWarning: you are using the root account. You may harm your system 💀\e[0m"
-echo DONE! 
+
+echo DONE! 
